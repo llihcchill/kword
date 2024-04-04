38049fcd87f736d0eb9f3b6d6fb93419f3064e77
llihcchill <125551072+llihcchill@users.noreply.github.com>
2024-04-04 16:21:37 +1000

    Update kword.py
     - Updated flag detection method by going over each sys.argv[] in a for loop so they can be placed anywhere

--- kword.py
+++ kword.py
@@ -1,4 +1,5 @@
 import sys
+import io
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,13 +12,40 @@ def webscrape(url):
 
     return '\n'.join(soup.stripped_strings)
 
+
 help = "KWORD HELP \n \n ======== \n \n -u: URL to be webscraped \n -ai: Keyword to be expanded upon via an AI API \n -o: Makes an output to a file \n \n Example usage: python kword.py -u <host url> -o list.txt"
 
-if sys.argv[1] == "--help":
-    print(help)
+h = False
+h_index = 0
+url = False
+u_index = 0
+output = False
+o_index = 0
 
-if sys.argv[1] == "-h":
-    print(help)
+for i in range(1, len(sys.argv)):
+    if sys.argv[i] == "--help" or sys.argv[i] == "-h":
+        h = True
+        h_index = i
+        
 
-if sys.argv[1] == "-u":
-    print(webscrape(sys.argv[2]))
+    if sys.argv[i] == "-o":
+        output = True
+        o_index = i
+    
+    if sys.argv[i] == "-u":
+        url = True
+        u_index = i
+
+def run():
+    if h == True:
+        print(help)
+        return
+
+    if output != True:
+        print(webscrape(sys.argv[u_index]))
+        return
+
+    with open(str(sys.argv[o_index + 1]), "w") as file:
+        file.write(webscrape(sys.argv[u_index]))
+
+run()
