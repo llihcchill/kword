7d3dbe43ec85b6d69e711ee29325bf5ea1023ca6
llihcchill <125551072+llihcchill@users.noreply.github.com>
2024-04-05 08:39:22 +1000

    Update kword.py
     - Made webscrape_filter() function that filters out words used in a seperate .txt file (pretty much works)

--- kword.py
+++ kword.py
@@ -1,19 +1,6 @@
 import sys
-import io
 import requests
 from bs4 import BeautifulSoup
-
-def webscrape(url):
-    website_content = requests.get(url).content
-    soup = BeautifulSoup(website_content, "html.parser")
-    
-    for data in soup(["style", "script"]):
-        data.decompose()
-
-    return '\n'.join(soup.stripped_strings)
-
-
-help = "KWORD HELP \n \n ======== \n \n -u: URL to be webscraped \n -ai: Keyword to be expanded upon via an AI API \n -o: Makes an output to a file \n \n Example usage: python kword.py -u <host url> -o list.txt"
 
 h = False
 h_index = 0
@@ -22,11 +9,36 @@ u_index = 0
 output = False
 o_index = 0
 
+def webscrape(url):
+    website_content = requests.get(url).text
+    soup = BeautifulSoup(website_content, "html.parser")
+    
+    for data in soup(["style", "script"]):
+        data.decompose()
+
+    return '\n'.join(soup.stripped_strings)
+
+def webscrape_filter():
+    data = webscrape(sys.argv[u_index + 1])
+    data = str(data.lower())
+    filter = open("filter.txt", "r")
+
+    for line in range(1, 49):
+        filter_word = str(filter.readline())
+
+        if data.find(filter_word[:]) > 0:
+            data = data.replace(filter_word, "\n")
+
+    return data
+
+
+help = "KWORD HELP \n \n ======== \n \n -u: URL to be webscraped \n -ai: Keyword to be expanded upon via an AI API \n -o: Makes an output to a file \n \n Example usage: python kword.py -u <host url> -o list.txt"
+
 for i in range(1, len(sys.argv)):
+
     if sys.argv[i] == "--help" or sys.argv[i] == "-h":
         h = True
         h_index = i
-        
 
     if sys.argv[i] == "-o":
         output = True
@@ -36,16 +48,21 @@ for i in range(1, len(sys.argv)):
         url = True
         u_index = i
 
+    if sys.argv[i] == "-r":
+        related = True
+        r_index = i
+
 def run():
     if h == True:
         print(help)
         return
 
     if output != True:
-        print(webscrape(sys.argv[u_index]))
+        print(webscrape_filter())
         return
 
     with open(str(sys.argv[o_index + 1]), "w") as file:
-        file.write(webscrape(sys.argv[u_index]))
+        file.write(webscrape_filter())
+        print("List successfully created!")
 
 run()
